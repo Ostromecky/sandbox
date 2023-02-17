3754fba1aeaf1ab728b4a5dd183aa8d0910a8bb4
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-17 15:12:33 +0100

    test: Test assets, routing

---
+++ src/app/page/page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-page',
+    template: '<h1>Welcome to the Page</h1>'
+})
+
+export class PageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
