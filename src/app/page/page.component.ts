9fcf896bff2529bbe8ea1f19ce1b8f7d84ef5764
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-20 17:28:45 +0100

    test: Test logo stylings

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector: 'app-page',
-    template: '<h1>Welcome to the Page</h1>'
+  selector: "app-page",
+  template: `
+    <h1>Welcome to the Page</h1>
+    <div class="logo-container">
+      <img src="assets/icotera-logo-dark.svg" />
+    </div>
+  `,
 })
-
 export class PageComponent implements OnInit {
-    constructor() { }
+  constructor() {}
 
-    ngOnInit() { }
+  ngOnInit() {}
 }
