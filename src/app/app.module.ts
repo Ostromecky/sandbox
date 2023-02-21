0ce3a2538705c819ad9ba937b64651596a2146ec
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-21 17:39:33 +0100

    chore: Add Eslint, prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { NxWelcomeComponent } from "./nx-welcome.component";
-import { PageComponent } from "./page/page.component";
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, PageComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: "welcome", component: NxWelcomeComponent },
-      { path: "page", component: PageComponent },
-      { path: "**", redirectTo: "welcome", pathMatch: 'full' },
-    ]),
+      { path: 'welcome', component: NxWelcomeComponent },
+      { path: 'page', component: PageComponent },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-page",
+  selector: 'app-page',
   template: `
     <h1>Welcome to the Page</h1>
     <div class="ico-logo-container">
       <img src="assets/icotera-logo-dark.svg" />
     </div>
-  `,
+  `
 })
 export class PageComponent implements OnInit {
   constructor() {}
