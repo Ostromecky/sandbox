8189fb32631375a46b7d1447bd7d7540ce372366
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-28 10:30:06 +0100

    Revert "feat: Add three-js library"
    This reverts commit 3287fde489b518efaed345dd5e8f8425f81fc67a.

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,18 +1,16 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { extend, NgtCanvas } from 'angular-three';
-import * as THREE from 'three';
-import { SceneGraphComponent } from '../components/scene-graph.component';
+import { Component, OnInit } from '@angular/core';
 
-extend(THREE);
 @Component({
   selector: 'app-page',
-  template: `<ngt-canvas [sceneGraph]="SceneGraph" />`,
-  imports: [NgtCanvas],
-  standalone: true,
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  template: `
+    <h1>Welcome to the Page</h1>
+    <div class="ico-logo-container">
+      <img src="assets/icotera-logo-dark.svg" />
+    </div>,
+  `,
+  standalone: true
 })
 export class PageComponent implements OnInit {
-  protected readonly SceneGraph = SceneGraphComponent;
   constructor() {}
 
   ngOnInit() {}
