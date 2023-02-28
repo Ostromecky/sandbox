aef89fdb0c8aab4c3274478b3886033d7f792fbf
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-28 09:54:26 +0100

    feat: Add bounds to camera

--- src/app/components/scene-graph.component.ts
+++ src/app/components/scene-graph.component.ts
@@ -1,9 +1,11 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { extend, NgtBeforeRenderEvent } from 'angular-three';
-import { AmbientLight, SpotLight, PointLight } from 'three';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { extend, NgtArgs, NgtBeforeRenderEvent, NgtStore } from 'angular-three';
+import { AmbientLight, PointLight, SpotLight, Loader } from 'three';
+import { OrbitControls } from 'three-stdlib';
 import { CubeComponent } from './cube.component';
 
 extend({ AmbientLight, SpotLight, PointLight });
+extend({ OrbitControls });
 
 @Component({
   standalone: true,
@@ -13,11 +15,15 @@ extend({ AmbientLight, SpotLight, PointLight });
     <ngt-point-light [position]="-10" />
     <app-cube [position]="[1.5, 0, 0]" />
     <app-cube [position]="[-1.5, 0, 0]" />
+    <ngt-orbit-controls *args="[camera, glDom]" [enableDamping]="true" [minAzimuthAngle]="45" [maxAzimuthAngle]="90" />
   `,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CubeComponent]
+  imports: [CubeComponent, NgtArgs]
 })
 export class SceneGraphComponent {
+  private readonly store = inject(NgtStore);
+  protected readonly camera = this.store.get('camera');
+  protected readonly glDom = this.store.get('gl', 'domElement');
   onBeforeRender(event: NgtBeforeRenderEvent<THREE.Mesh>) {
     event.object.rotation.x += 0.01;
   }
