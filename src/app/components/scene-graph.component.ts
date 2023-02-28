9226962e7035df253b9b898fc4873e7ed5e65618
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-28 10:29:53 +0100

    Revert "feat: Add bounds to camera"
    This reverts commit aef89fdb0c8aab4c3274478b3886033d7f792fbf.

--- src/app/components/scene-graph.component.ts
+++ src/app/components/scene-graph.component.ts
@@ -1,11 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
-import { extend, NgtArgs, NgtBeforeRenderEvent, NgtStore } from 'angular-three';
-import { AmbientLight, PointLight, SpotLight, Loader } from 'three';
-import { OrbitControls } from 'three-stdlib';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { extend, NgtBeforeRenderEvent } from 'angular-three';
+import { AmbientLight, SpotLight, PointLight } from 'three';
 import { CubeComponent } from './cube.component';
 
 extend({ AmbientLight, SpotLight, PointLight });
-extend({ OrbitControls });
 
 @Component({
   standalone: true,
@@ -15,15 +13,11 @@ extend({ OrbitControls });
     <ngt-point-light [position]="-10" />
     <app-cube [position]="[1.5, 0, 0]" />
     <app-cube [position]="[-1.5, 0, 0]" />
-    <ngt-orbit-controls *args="[camera, glDom]" [enableDamping]="true" [minAzimuthAngle]="45" [maxAzimuthAngle]="90" />
   `,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CubeComponent, NgtArgs]
+  imports: [CubeComponent]
 })
 export class SceneGraphComponent {
-  private readonly store = inject(NgtStore);
-  protected readonly camera = this.store.get('camera');
-  protected readonly glDom = this.store.get('gl', 'domElement');
   onBeforeRender(event: NgtBeforeRenderEvent<THREE.Mesh>) {
     event.object.rotation.x += 0.01;
   }
