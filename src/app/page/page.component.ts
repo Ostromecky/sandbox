0625a6a2e0af6932b69e9d0a544cd35a8e77046c
Paweł Ostromecki <pawel.ostromecki@god.dev>
2023-02-28 15:08:40 +0100

    feat: Add new angular-three new way with loader

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NgtCanvas } from 'angular-three';
+import { SceneGraphComponent } from '../components/scene-graph.component';
 
 @Component({
   selector: 'app-page',
-  template: `
-    <h1>Welcome to the Page</h1>
-    <div class="ico-logo-container">
-      <img src="assets/icotera-logo-dark.svg" />
-    </div>,
-  `,
+  template: `<ngt-canvas [sceneGraph]="SceneGraph" />`,
+  imports: [NgtCanvas],
   standalone: true
 })
 export class PageComponent implements OnInit {
+  protected readonly SceneGraph = SceneGraphComponent;
   constructor() {}
 
   ngOnInit() {}
